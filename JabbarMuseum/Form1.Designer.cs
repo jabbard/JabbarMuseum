411e4fbd3cc20ad7f62727b6fef2dff97d92fbab
jabbard <Potter123>
2018-12-24 03:17:35 +0545

    Add project files.

---
+++ JabbarMuseum/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace JabbarMuseum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMuseumName = new System.Windows.Forms.Label();
+            this.groupBoxData = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radBtnMale = new System.Windows.Forms.RadioButton();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.labelNumber = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelOcupation = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.groupBoxData.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelMuseumName
+            // 
+            this.labelMuseumName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMuseumName.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMuseumName.Location = new System.Drawing.Point(329, 22);
+            this.labelMuseumName.Name = "labelMuseumName";
+            this.labelMuseumName.Size = new System.Drawing.Size(350, 50);
+            this.labelMuseumName.TabIndex = 0;
+            this.labelMuseumName.Text = "JABBAR MUSEUM";
+            this.labelMuseumName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelMuseumName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBoxData
+            // 
+            this.groupBoxData.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBoxData.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBoxData.Controls.Add(this.btnClearAll);
+            this.groupBoxData.Controls.Add(this.btnSave);
+            this.groupBoxData.Controls.Add(this.radioButton2);
+            this.groupBoxData.Controls.Add(this.radBtnMale);
+            this.groupBoxData.Controls.Add(this.labelGender);
+            this.groupBoxData.Controls.Add(this.comboBox2);
+            this.groupBoxData.Controls.Add(this.txtPhone);
+            this.groupBoxData.Controls.Add(this.txtAddress);
+            this.groupBoxData.Controls.Add(this.txtName);
+            this.groupBoxData.Controls.Add(this.labelNumber);
+            this.groupBoxData.Controls.Add(this.labelAddress);
+            this.groupBoxData.Controls.Add(this.labelOcupation);
+            this.groupBoxData.Controls.Add(this.labelName);
+            this.groupBoxData.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxData.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBoxData.Location = new System.Drawing.Point(123, 82);
+            this.groupBoxData.Name = "groupBoxData";
+            this.groupBoxData.Size = new System.Drawing.Size(800, 431);
+            this.groupBoxData.TabIndex = 14;
+            this.groupBoxData.TabStop = false;
+            this.groupBoxData.Text = "Visitor\'s data";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(509, 277);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 26);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radBtnMale
+            // 
+            this.radBtnMale.AutoSize = true;
+            this.radBtnMale.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radBtnMale.Location = new System.Drawing.Point(339, 277);
+            this.radBtnMale.Name = "radBtnMale";
+            this.radBtnMale.Size = new System.Drawing.Size(64, 26);
+            this.radBtnMale.TabIndex = 23;
+            this.radBtnMale.TabStop = true;
+            this.radBtnMale.Text = "Male";
+            this.radBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGender.Location = new System.Drawing.Point(111, 269);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(82, 23);
+            this.labelGender.TabIndex = 22;
+            this.labelGender.Text = "Gender:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(339, 216);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(280, 31);
+            this.comboBox2.TabIndex = 21;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(339, 107);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(280, 32);
+            this.txtPhone.TabIndex = 20;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(339, 162);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(280, 32);
+            this.txtAddress.TabIndex = 19;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(339, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(280, 32);
+            this.txtName.TabIndex = 18;
+            // 
+            // labelNumber
+            // 
+            this.labelNumber.AutoSize = true;
+            this.labelNumber.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumber.Location = new System.Drawing.Point(111, 107);
+            this.labelNumber.Name = "labelNumber";
+            this.labelNumber.Size = new System.Drawing.Size(107, 23);
+            this.labelNumber.TabIndex = 17;
+            this.labelNumber.Text = "Phone No.:";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress.Location = new System.Drawing.Point(111, 162);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(89, 23);
+            this.labelAddress.TabIndex = 16;
+            this.labelAddress.Text = "Address:";
+            // 
+            // labelOcupation
+            // 
+            this.labelOcupation.AutoSize = true;
+            this.labelOcupation.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOcupation.Location = new System.Drawing.Point(111, 216);
+            this.labelOcupation.Name = "labelOcupation";
+            this.labelOcupation.Size = new System.Drawing.Size(115, 23);
+            this.labelOcupation.TabIndex = 15;
+            this.labelOcupation.Text = "Occupation:";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(111, 48);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(67, 23);
+            this.labelName.TabIndex = 14;
+            this.labelName.Text = "Name:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(163, 348);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(176, 49);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(413, 348);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(176, 49);
+            this.btnClearAll.TabIndex = 26;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 582);
+            this.Controls.Add(this.groupBoxData);
+            this.Controls.Add(this.labelMuseumName);
+            this.Name = "Form1";
+            this.Text = "Museum";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxData.ResumeLayout(false);
+            this.groupBoxData.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMuseumName;
+        private System.Windows.Forms.GroupBox groupBoxData;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radBtnMale;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label labelNumber;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelOcupation;
+        private System.Windows.Forms.Label labelName;
+    }
+}
+
